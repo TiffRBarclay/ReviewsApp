8c38558c6bc1e8cc85b9e5b234eeaca94c1b4c36
Tiffany Barclay <tiffany@barent.co.nz>
2020-08-27 16:07:22 +1200

    Add styling to HomeStack headers

--- routes/HomeStack.js
+++ routes/HomeStack.js
@@ -7,9 +7,24 @@ import ReviewDetails from "../screens/ReviewDetails";
 const Stack = createStackNavigator();
 
 const HomeStack = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Home" component={Home} />
-    <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
+  <Stack.Navigator
+    screenOptions={{
+      headerTintColor: "#444",
+      headerStyle: {
+        backgroundColor: "#eee",
+      },
+    }}
+  >
+    <Stack.Screen
+      name="Home"
+      component={Home}
+      options={{ title: "Game Zone" }}
+    />
+    <Stack.Screen
+      name="ReviewDetails"
+      component={ReviewDetails}
+      options={{ title: "Review Details" }}
+    />
   </Stack.Navigator>
 );
 
