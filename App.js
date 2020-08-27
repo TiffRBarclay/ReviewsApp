d2ca65ce580678e54c47e5df7745a30dc5ae1b1a
Tiffany Barclay <tiffany@barent.co.nz>
2020-08-27 15:29:56 +1200

    Add outer navigation to app.js

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+import { NavigationContainer } from "@react-navigation/native";
 
-import Home from "./screens/Home";
+import HomeStack from "./routes/HomeStack";
 
 const getFonts = () => {
   return Font.loadAsync({
@@ -15,7 +16,11 @@ export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   if (fontsLoaded) {
-    return <Home />;
+    return (
+      <NavigationContainer>
+        <HomeStack />
+      </NavigationContainer>
+    );
   } else {
     return (
       <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
