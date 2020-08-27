5da7ea06bcadc596b0985f715d819d0c7b7fdb0c
Tiffany Barclay <tiffany@barent.co.nz>
2020-08-27 15:30:26 +1200

    Create HomeStack

---
+++ routes/HomeStack.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "../screens/Home";
+import ReviewDetails from "../screens/ReviewDetails";
+
+const Stack = createStackNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
+  </Stack.Navigator>
+);
+
+export default HomeStack;
